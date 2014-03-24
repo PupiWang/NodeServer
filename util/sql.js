f17d77d51e3f0069a14a8a133b0b8262d3f692b8
Pupi <dreamjl@live.cn>
2014-03-24 14:54:04 +0800

    sql连接占用错误

--- util/sql.js
+++ util/sql.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'onevoout.mysql.rds.aliyuncs.com',
-    user     : 'pupi',
-    database : 'onevo',
-    password : 'PUPI_1'
-});
 
 exports.execute = function (sql, callback) {
-  connection.connect(function () {
-    console.log(sql);
-  });
-  connection.query(sql, callback);
-  connection.end();
+    var connection = mysql.createConnection({
+        host     : 'onevoout.mysql.rds.aliyuncs.com',
+        user     : 'pupi',
+        database : 'onevo',
+        password : 'PUPI_1'
+    });
+    connection.connect(function () {
+        console.log(sql);
+    });
+    connection.query(sql, callback);
+    connection.end();
 };
 
 var userSQL = {
