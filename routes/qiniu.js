19f8a75efa9389fa148df01cb80315226d31e0ce
Pupi <dreamjl@live.cn>
2014-05-04 11:22:47 +0800

    format the code,fix reference error

--- routes/qiniu.js
+++ routes/qiniu.js
@@ -9,14 +9,14 @@ qiniu.conf.SECRET_KEY = 'up-RiROP73u2M8hf94ysPRFf9OlDGr07Xr426r9R';
  * @return {string} url    资源链接
  */
 var getDownloadUrl = function (domain, key) {
-  if (!key) {
-    return '';
-  }
+    if (!key) {
+        return '';
+    }
 
-  var baseUrl = qiniu.rs.makeBaseUrl(domain, key),
-    policy = new qiniu.rs.GetPolicy();
+    var baseUrl = qiniu.rs.makeBaseUrl(domain, key),
+        policy = new qiniu.rs.GetPolicy();
 
-  return policy.makeRequest(baseUrl);
+    return policy.makeRequest(baseUrl);
 };
 
 /**
@@ -26,25 +26,25 @@ var getDownloadUrl = function (domain, key) {
  * @return {string} url    资源链接
  */
 exports.getUrl = function (key) {
-  var domain = 'ov-orange-private.u.qiniudn.com';
-  return getDownloadUrl(domain, key);
+    var domain = 'ov-orange-private.u.qiniudn.com';
+    return getDownloadUrl(domain, key);
 };
 
 /**
  * 获取上传令牌
  */
 exports.getUploadToken = function (req, res) {
-  var putPolicy = new qiniu.rs.PutPolicy('ov-orange-private');
-  //七牛回调链接
-  putPolicy.callbackUrl = 'http://115.29.179.7/uploadCallback';
-  //魔法变量和自定义变量
-  putPolicy.callbackBody = 'bucket=$(bucket)&etag=$(etag)&fname=$(fname)&fsize=$(fsize)' +
-                           '&mimeType=$(mimeType)&imageInfo=$(imageInfo)' +
-                           '&width=$(imageInfo.width)&height=$(imageInfo.height)' +
-                           '&format=$(imageInfo.format)&endUser=$(endUser)' +
-                           '&exif=$(exif)&ApertureValue=$(exif.ApertureValue)&val=$(exif.ApertureValue.val)' +
-                           '&user_id=$(x:user_id)&device_id=$(x:device_id)&time=$(x:time)';
-  res.send(putPolicy.token());
+    var putPolicy = new qiniu.rs.PutPolicy('ov-orange-private');
+    //七牛回调链接
+    putPolicy.callbackUrl = 'http://115.29.179.7/uploadCallback';
+    //魔法变量和自定义变量
+    putPolicy.callbackBody = 'bucket=$(bucket)&etag=$(etag)&fname=$(fname)&fsize=$(fsize)' +
+        '&mimeType=$(mimeType)&imageInfo=$(imageInfo)' +
+        '&width=$(imageInfo.width)&height=$(imageInfo.height)' +
+        '&format=$(imageInfo.format)&endUser=$(endUser)' +
+        '&exif=$(exif)&ApertureValue=$(exif.ApertureValue)&val=$(exif.ApertureValue.val)' +
+        '&user_id=$(x:user_id)&device_id=$(x:device_id)&time=$(x:time)';
+    res.send(putPolicy.token());
 };
 
 /**
@@ -53,7 +53,7 @@ exports.getUploadToken = function (req, res) {
 exports.getAccessToken = function (req, res) {
     var crypto = require('crypto');
     //加密对象
-    var hmacSha1 = crypto.createHmac('sha1',qiniu.conf.SECRET_KEY);
+    var hmacSha1 = crypto.createHmac('sha1', qiniu.conf.SECRET_KEY);
     //签名
     var sign = hmacSha1.update('/pfop/\nbucket=ov-orange-private&key=wangzhi.mp4&fops=avthumb%2Fm3u8%2Fab%2F320k%2Fr%2F24&notifyURL=http%3A%2F%2Ffake.com%2Fqiniu%2Fnotify&');
     //编码
@@ -71,80 +71,80 @@ exports.getAccessToken = function (req, res) {
  */
 exports.uploadCallback = function (req, res) {
 
-  var sql = require('../util/sql');
+    var sql = require('../util/sql');
 
-  var s, i;
+    var s, i;
 
-  var url;
+    var url;
 
-  if (req.body.mimeType === 'image/jpeg') {
-    //如果上传的资源是图片
-    //picture_device
-    s = 'INSERT INTO picture_device (`key`, id_device, datetime_create) VALUES ("' +
+    if (req.body.mimeType === 'image/jpeg') {
+        //如果上传的资源是图片
+        //picture_device
+        s = 'INSERT INTO picture_device (`key`, id_device, datetime_create) VALUES ("' +
             req.body.etag + '", "' + req.body.device_id + '", ' + req.body.time * 1000 + ')';
-
-    sql.execute(s, function (err) {
-      if (err) {
-        throw err;
-      }
-    });
-
-    //resource_picture
-    s = 'INSERT INTO resource_picture (bucket, `key`, name, size, type, datetime_upload, width, height) VALUES ("' +
-        req.body.bucket + '","' + req.body.etag + '","' + req.body.fname + '","' +
-        req.body.fsize + '","' + req.body.mimeType + '",' + req.body.time * 1000 + ',' +
-        req.body.width + ',' + req.body.height + ')';
-
-    sql.execute(s, function (err) {
-        if (err) {
-            throw err;
-        }
-    });
-
-    var alarmMessageUtil = require('../util/alarmUtil');
-    alarmUtil.addAlarm(req.body.device_id, req.body.etag);
-
-    res.end();
-
-  } else if (req.body.mimeType === 'video/mp4') {
-    //如果上传的资源是视频
-    ////在video_device、resource_video两个表中插入数据
-    s = 'INSERT INTO video_device (`key`, id_device, datetime_create) VALUES ("' +
-            req.body.etag + '", "' + req.body.device_id + '", ' + req.body.time * 1000 + ')';
-
-    sql.execute(s, function (err) {
-      if (err) {
-        throw err;
-      } else {
-        s = 'INSERT INTO resource_video (bucket, `key`, name, size, type, datetime_upload) VALUES ("' +
-          req.body.bucket + '","' + req.body.etag + '","' + req.body.fname + '","' +
-          req.body.fsize + '","' + req.body.mimeType + '",' + req.body.time * 1000 + ')';
 
         sql.execute(s, function (err) {
-          if (err) {
-            throw err;
-          } else {
-            //两条插入操作成功执行
-            //构造视频链接，根据给定的user_id找到client_sockets发送信息
-            url = getDownloadUrl('ov-orange-private.u.qiniudn.com', req.body.etag);
-
-            for (i = client_sockets.length - 1; i >= 0; i--) {
-              var c = client_sockets[i];
-              if (c.user_id === req.body.user_id) {
-                if (c.write) {
-                  var protbufConvertor = require('./../socketServer').protbufConvertor;
-                  protbufConvertor(c, {from: req.body.device_id, to: req.body.user_id, info: url, responseStatus: 1, cmd: 2});
-                } else {
-                  c.emit('data', {'type': 'video', 'url': url});
-                }
-              }
+            if (err) {
+                throw err;
             }
-
-          }
-
         });
-      }
-    });
-  }
+
+        //resource_picture
+        s = 'INSERT INTO resource_picture (bucket, `key`, name, size, type, datetime_upload, width, height) VALUES ("' +
+            req.body.bucket + '","' + req.body.etag + '","' + req.body.fname + '","' +
+            req.body.fsize + '","' + req.body.mimeType + '",' + req.body.time * 1000 + ',' +
+            req.body.width + ',' + req.body.height + ')';
+
+        sql.execute(s, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+
+        var alarmUtil = require('../util/alarmUtil');
+        alarmUtil.addAlarm(req.body.device_id, req.body.etag);
+
+        res.end();
+
+    } else if (req.body.mimeType === 'video/mp4') {
+        //如果上传的资源是视频
+        ////在video_device、resource_video两个表中插入数据
+        s = 'INSERT INTO video_device (`key`, id_device, datetime_create) VALUES ("' +
+            req.body.etag + '", "' + req.body.device_id + '", ' + req.body.time * 1000 + ')';
+
+        sql.execute(s, function (err) {
+            if (err) {
+                throw err;
+            } else {
+                s = 'INSERT INTO resource_video (bucket, `key`, name, size, type, datetime_upload) VALUES ("' +
+                    req.body.bucket + '","' + req.body.etag + '","' + req.body.fname + '","' +
+                    req.body.fsize + '","' + req.body.mimeType + '",' + req.body.time * 1000 + ')';
+
+                sql.execute(s, function (err) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        //两条插入操作成功执行
+                        //构造视频链接，根据给定的user_id找到client_sockets发送信息
+                        url = getDownloadUrl('ov-orange-private.u.qiniudn.com', req.body.etag);
+
+                        for (i = client_sockets.length - 1; i >= 0; i--) {
+                            var c = client_sockets[i];
+                            if (c.user_id === req.body.user_id) {
+                                if (c.write) {
+                                    var protbufConvertor = require('./../socketServer').protbufConvertor;
+                                    protbufConvertor(c, {from: req.body.device_id, to: req.body.user_id, info: url, responseStatus: 1, cmd: 2});
+                                } else {
+                                    c.emit('data', {'type': 'video', 'url': url});
+                                }
+                            }
+                        }
+
+                    }
+
+                });
+            }
+        });
+    }
 
 };
